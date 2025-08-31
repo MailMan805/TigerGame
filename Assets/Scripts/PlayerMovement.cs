e8795bcfa81248f670d102b38c5edd517bb44214
nathanf0603 <nf263202@my.stchas.edu>
2025-08-30 21:45:02 -0500

    Player Controller
    presidentObama.mat is imperative to the success of the player controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 4.0f;
+    public float jumpForce = 300f;
+    public float mouseSensitivity = 2.0f;
+    public float gravity = -9.81f;
+
+    public Transform playerCamera;
+
+    private CharacterController characterController;
+    private float verticalRotation;
+    private float verticalVelocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Looking Around
+        float horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, horizontalRotation, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+
+        playerCamera.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        // Jumping
+
+        isGrounded = characterController.isGrounded;
+
+        if (isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f; // keeps player grounded
+        }
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            verticalVelocity = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        // Movement
+        float moveNS = Input.GetAxis("Vertical");
+        float moveEW = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * moveEW + transform.forward * moveNS;
+        move *= movementSpeed;
+
+        move.y = verticalVelocity;
+
+        characterController.Move(move * Time.deltaTime);
+
+    }
+}
