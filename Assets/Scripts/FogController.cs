3f5ce6fd6753f7ccf7840f3e9eacd40e960f0222
Whyinds <whyinds@gmail.com>
2025-09-14 20:00:15 -0500

    Fog Color mechanic
    Can now be set to a range between white and red

--- Assets/Scripts/FogController.cs
+++ Assets/Scripts/FogController.cs
@@ -19,10 +19,13 @@ public enum FogDensity
 public class FogController : MonoBehaviour
 {
 
+    public FogDensity density = FogDensity.NORMAL;
+
     Volume volume;
     Fog fog;
 
-    public FogDensity density = FogDensity.NORMAL;
+    Gradient fogColorGradient = new Gradient();
+
     public float fogTransitionSpeed = 0.2f;
 
     float newDistance = 0;
@@ -33,9 +36,9 @@ public class FogController : MonoBehaviour
 
     List<float> fogDenseList = new List<float>();
 
-    const int FOGDENSITYCOUNT = 7;
-
     float time = 0f;
+
+    const float MAXREDCOLORAMOUNT = 0.4f; // How much R in RGB the fog color can be in float percentage. 0~255 == 0.0f~1.0f
 
     // Start is called before the first frame update
     void Start()
@@ -48,23 +51,40 @@ public class FogController : MonoBehaviour
         }
 
         MakeFogList();
+        CreateColorGradient();
 
         newDistance = fogDenseList[(int)FogDensity.NORMAL];
         previousDistance = fogDenseList[(int)FogDensity.NORMAL];
     }
 
+    // Divides the fog densities by the fogDensity enums. Spreads all densities evenly.
     void MakeFogList()
     {
         fogDenseList.Clear();
 
-        var fogDistanceSeparator = maxFogDistance / FOGDENSITYCOUNT;
+        var fogDensityCount = System.Enum.GetValues(typeof(FogDensity)).Length;
 
-        fogDenseList.Add(maxFogDistance);
-        for (int i = 1; i < FOGDENSITYCOUNT - 1; i++)
+        var fogDistanceSeparator = maxFogDistance / fogDensityCount;
+
+        for (int i = 0; i < fogDensityCount - 1; i++)
         {
             fogDenseList.Add(maxFogDistance - (fogDistanceSeparator * i));
         }
         fogDenseList.Add(minFogDinstance);
+    }
+
+    void CreateColorGradient()
+    {
+        // Blend color from white at 0% to red at 100%
+        var colors = new GradientColorKey[2];
+        colors[0] = new GradientColorKey(Color.white, 0.0f);
+        colors[1] = new GradientColorKey(new Color(MAXREDCOLORAMOUNT,0,0), 1.0f);
+
+        var alphas = new GradientAlphaKey[2];
+        alphas[0] = new GradientAlphaKey(1.0f, 0.0f);
+        alphas[1] = new GradientAlphaKey(1.0f, 1.0f);
+
+        fogColorGradient.SetKeys(colors, alphas);
     }
 
     private void Update()
@@ -83,12 +103,32 @@ public class FogController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Sets the fog density with the appropriate FogDensity enum level.
+    /// </summary>
+    /// <param name="fogDensity"></param>
     public void SetFogDensity(FogDensity fogDensity)
     {
         time = 0f;
         density = fogDensity;
         previousDistance = fog.meanFreePath.value;
         newDistance = fogDenseList[(int)density];
+    }
+
+    /// <summary>
+    /// Sets the fog color to be between white (0.0f) and red (1.0f).
+    /// </summary>
+    /// <param name="evilFogColorAmount"></param>
+    public void SetEvilFogColorAmount(float evilFogColorAmount)
+    {
+        if (evilFogColorAmount > 1f) { evilFogColorAmount = 1.0f; }
+        if (evilFogColorAmount < 0f) { evilFogColorAmount = 0.0f; }
+
+        var newFogColor = fogColorGradient.Evaluate(evilFogColorAmount);
+        if (volume.profile.TryGet<Fog>(out fog))
+        {
+            fog.albedo.value = newFogColor;
+        }
     }
 
     void TestFog()
@@ -129,6 +169,35 @@ public class FogController : MonoBehaviour
             SetFogDensity(FogDensity.NONE);
             Debug.Log("Set Fog Density to none");
         }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            SetEvilFogColorAmount(0f);
+            Debug.Log("Set Fog Color to white");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            SetEvilFogColorAmount(0.25f);
+            Debug.Log("Set Fog Density to mostly white");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            SetEvilFogColorAmount(0.5f);
+            Debug.Log("Set Fog Color to half-mix");
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            SetEvilFogColorAmount(0.75f);
+            Debug.Log("Set Fog Density to mostly red");
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            SetEvilFogColorAmount(1f);
+            Debug.Log("Set Fog Density to red");
+        }
     }
+
+   
 
 }
